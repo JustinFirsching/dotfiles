7e77df8baa20bae8bebf45a0cc84310a57b6608a
Justin Firsching <JustinF52399@gmail.com>
2021-09-30 23:02:02 -0400

    Update default terminal and add JAVA_HOME

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -1,7 +1,9 @@
 export EDITOR=nvim
-export TERMINAL=kitty
+export TERMINAL=st
 export BROWSER=brave
 export PS1="%n@%M:%c$ "
 
 PATH=${PATH}:$HOME/.local/bin
 export PATH
+
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/default-runtime}
