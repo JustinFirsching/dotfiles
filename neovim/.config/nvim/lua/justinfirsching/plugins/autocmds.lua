6399370397bd42311e3a0d4e9f87c639a5720bb2
Justin Firsching <JustinF52399@gmail.com>
2021-09-30 23:03:37 -0400

    neovim(Add autocmd to reload .Xresources on write)

--- neovim/.config/nvim/lua/justinfirsching/plugins/autocmds.lua
+++ neovim/.config/nvim/lua/justinfirsching/plugins/autocmds.lua
@@ -1,1 +1,2 @@
-vim.cmd('autocmd BufWritePost plugins.lua source <afile> | PackerCompile')
+vim.cmd('autocmd BufWritePost packer.lua source <afile> | PackerCompile')
+vim.cmd('autocmd BufWritePost .Xresources silent !xrdb <afile>')
