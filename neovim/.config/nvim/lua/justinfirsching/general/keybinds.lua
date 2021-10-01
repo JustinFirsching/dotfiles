920fd6a86db2a266fd5f0f159bf43d5c3545c8de
Justin Firsching <JustinF52399@gmail.com>
2021-09-30 22:48:55 -0400

    nvim(Add Ctrl-C to escape insert bind back)

--- neovim/.config/nvim/lua/justinfirsching/general/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/general/keybinds.lua
@@ -1,19 +1,22 @@
-local bind = function(key, f)
-    vim.api.nvim_set_keymap('n', key, f, { noremap = true, silent = true })
+local bind = function(mode, key, f)
+    vim.api.nvim_set_keymap(mode, key, f, { noremap = true, silent = true })
 end
 
 -- Set the leader key
 vim.g.mapleader = " "
 
+-- Exit Insert Mode
+bind('i', '<C-c>', '<Esc>')
+
 -- Undo Menu
-bind('<leader>u', '<cmd>UndotreeShow<CR>')
+bind('n', '<leader>u', '<cmd>UndotreeShow<CR>')
 -- File Finder
-bind('<leader>rf', '<cmd>Telescope find_files hidden=true<CR>')
+bind('n', '<leader>rf', '<cmd>Telescope find_files hidden=true<CR>')
 -- Buffer Reader
-bind('<leader>rb', '<cmd>Telescope buffers<CR>')
+bind('n', '<leader>rb', '<cmd>Telescope buffers<CR>')
 -- Old File Reader
-bind('<leader>ro', '<cmd>Telescope oldfiles<CR>')
+bind('n', '<leader>ro', '<cmd>Telescope oldfiles<CR>')
 -- Project Search
-bind('<leader>ps', '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grep Input> ") })<CR>')
+bind('n', '<leader>ps', '<cmd>lua require("telescope.builtin").grep_string({ search = vim.fn.input("Grep Input> ") })<CR>')
 
 return bind
