15da21b7071a7aa8fc064cfbff8e443f2f78292d
Justin Firsching <JustinF52399@gmail.com>
2021-10-02 14:34:50 -0400

    neovim(Fix theme)

--- neovim/.config/nvim/lua/justinfirsching/general/theme.lua
+++
@@ -1,7 +0,0 @@
--- Theme
-vim.g.colors_name = 'gruvbox'
-vim.cmd('set t_Co=256')
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.opt.background = 'dark'
-vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
--- End Theme
---
+++ neovim/.config/nvim/plugin/theme.vim
@@ -0,0 +1,5 @@
+set termguicolors
+set background=dark
+colorscheme gruvbox
+let g:gruvbox_contrast_dark='hard'
+highlight Normal guibg=NONE ctermbg=NONE
