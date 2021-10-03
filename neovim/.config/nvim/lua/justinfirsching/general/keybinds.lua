e5bb9abb489f3e45ebcd5c003174723d506dd75c
Justin Firsching <JustinF52399@gmail.com>
2021-10-03 13:18:39 -0400

    Neovim(Fix Ctrl-C bind)

--- neovim/.config/nvim/lua/justinfirsching/general/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/general/keybinds.lua
@@ -2,11 +2,15 @@ local bind = function(mode, key, f)
     vim.api.nvim_set_keymap(mode, key, f, { noremap = true, silent = true })
 end
 
+local bind_buf = function(mode, key, f)
+    vim.api.nvim_buf_set_keymap(0, mode, key, f, {noremap = true, silent = true})
+end
+
 -- Set the leader key
 vim.g.mapleader = " "
 
 -- Exit Insert Mode
-bind('i', '<C-c>', '<Esc>')
+bind_buf('i', '<C-c>', '<Esc>')
 
 -- Undo Menu
 bind('n', '<leader>u', '<cmd>UndotreeShow<CR>')
