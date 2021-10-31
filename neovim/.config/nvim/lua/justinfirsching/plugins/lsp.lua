e3dfee2da711b80ba856e7ac093a0143235b3ab5
Justin Firsching <JustinF52399@gmail.com>
2021-10-31 16:23:47 -0400

    feat(neovim): Telescope Configuration

--- neovim/.config/nvim/lua/justinfirsching/plugins/lsp.lua
+++ neovim/.config/nvim/lua/justinfirsching/plugins/lsp.lua
@@ -5,21 +5,32 @@ end
 local lsp_installer = require('nvim-lsp-installer')
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, 'n', ...) end
-
   local opts = { noremap=true, silent=true }
+  local buf_set_keymap = function(key, func) vim.api.nvim_buf_set_keymap(bufnr, 'n', key, func, opts) end
+  buf_set_keymap('<leader>vca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+  buf_set_keymap('<leader>vD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+  buf_set_keymap('<leader>vd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+  buf_set_keymap('<leader>vf', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>')
+  buf_set_keymap('<leader>vh', '<cmd>lua vim.lsp.buf.hover()<CR>')
+  buf_set_keymap('<leader>vi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  buf_set_keymap('<leader>vn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+  buf_set_keymap('<leader>vp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+  buf_set_keymap('<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  buf_set_keymap('<leader>vrr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  buf_set_keymap('<leader>vsd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+  buf_set_keymap('<leader>vsh', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
 
-  buf_set_keymap('<leader>vD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('<leader>vd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('<leader>vi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('<leader>vsh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('<leader>vrr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('<leader>vh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('<leader>vca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('<leader>vsd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('<leader>vn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('<leader>vp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  local has_telescope, _ = pcall(require, 'telescope')
+  if has_telescope then
+    -- Find Document Symbols
+    buf_set_keymap('<leader>vds', '<cmd>Telescope lsp_document_symbols<CR>')
+    -- Find Project Symbols (This will probably run slow)
+    buf_set_keymap('<leader>vps', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>')
+    -- Find Project Functions and Methods
+    buf_set_keymap('<leader>vpf', "<cmd>lua require('telescope.builtin').lsp_workspace_symbols{ symbols={'Function', 'Method'} }<CR>")
+    -- Find Project Classes, Enums and Structs
+    buf_set_keymap('<leader>vpc', "<cmd>lua require('telescope.builtin').lsp_workspace_symbols{ symbols={'Class', 'Enum', 'Struct'} }<CR>")
+  end
 end
 
 lsp_installer.on_server_ready(function(server)
---
+++ neovim/.config/nvim/lua/justinfirsching/plugins/telescope.lua
@@ -0,0 +1,59 @@
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+local Path = require('plenary.path')
+local os_sep = Path.path.sep
+
+local create_new_file = function(prompt_bufnr)
+  local is_dir = function(value)
+    return value:sub(-1, -1) == os_sep
+  end
+
+  local file = action_state.get_current_line()
+  if file == "" then
+    print(
+      "To create a new file or directory(add "
+        .. os_sep
+        .. " at the end of file) "
+        .. "write the desired new into the prompt and press <C-e>. "
+        .. "It works for not existing nested input as well."
+        .. "Example: this"
+        .. os_sep
+        .. "is"
+        .. os_sep
+        .. "a"
+        .. os_sep
+        .. "new_file.lua"
+    )
+    return
+  end
+
+  actions.close(prompt_bufnr)
+  if not is_dir(file) then
+    Path:new(file):touch { parents = true }
+    vim.cmd(string.format(":edit %s", file))
+  else
+    Path:new(file:sub(1, -2)):mkdir { parents = true }
+    vim.cmd(string.format(":Explore %s", file))
+  end
+end
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-e>"] = create_new_file,
+        ["<C-c>"] = false,
+      },
+      n = {
+        ["<C-c>"] = actions.close,
+        ["<C-e>"] = create_new_file,
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
+}
