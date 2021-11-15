54291665910dc679cff97d4c27667f8e4b81b7df
Justin Firsching <JustinFirsching@gmail.com>
2021-11-15 00:03:27 -0500

    fix(zsh): Reorder PATH

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -7,7 +7,7 @@ export PS1="%n@%M:%c$ "
 
 export GPG_TTY=$TTY
 
-PATH=${PATH}:$HOME/.local/bin
+PATH=$HOME/.local/bin:$PATH
 export PATH
 
 export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/default-runtime}
