9186be9e8ff2aedc222023458ae508fa87d4cd04
Justin Firsching <JustinFirsching@gmail.com>
2022-02-18 00:36:23 -0500

    Fix packer boostraping

--- neovim/.config/nvim/lua/justinfirsching/plugins/packer/init.lua
+++ neovim/.config/nvim/lua/justinfirsching/plugins/packer/init.lua
@@ -2,7 +2,9 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_boostrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
 return require('packer').startup(function(use)
@@ -69,4 +71,9 @@ return require('packer').startup(function(use)
     run = ':TSUpdate',
   }
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
