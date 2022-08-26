ffb4cb280ca0c4e3c7a0e1e7be130c68aafb0f81
Justin Firsching <JustinFirsching@gmail.com>
2022-08-26 11:42:43 -0400

    fix(zsh): Reorganize scripts to ensure correct run order

--- zsh/.config/zsh/10.git_prefix.zsh
+++ zsh/.config/zsh/10.git_prefix.zsh
@@ -5,10 +5,13 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
 # This line obtains information from the vcs.
 zstyle ':vcs_info:git*' formats " (%b)"
+
 precmd() {
     vcs_info
 }
+
 # Enable substitution in the prompt.
 setopt prompt_subst
+
 # Config for the prompt. PS1 synonym.
 export PS1='%n@%M:%c${vcs_info_msg_0_}$ '
---
+++ zsh/.config/zsh/README.md
@@ -0,0 +1,2 @@
+The files in this directory have been named according to the order they should
+be executed in.
