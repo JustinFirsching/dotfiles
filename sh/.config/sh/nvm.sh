db306899230840f69f47c1454b1d17ac7e25a1b1
Justin Firsching <JustinFirsching@gmail.com>
2023-11-05 11:07:05 -0500

    fix(sh): Better nvm shell config

--- sh/.config/sh/nvm.sh
+++ sh/.config/sh/nvm.sh
@@ -1,3 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if [ -d "$HOME/.nvm" ]; then
+    [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+    source /usr/share/nvm/nvm.sh
+    source /usr/share/nvm/bash_completion
+    source /usr/share/nvm/install-nvm-exec
+fi
