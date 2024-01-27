fd623a34fdf3130efc23b5a32f9e82556e27d2d4
Justin Firsching <JustinFirsching@gmail.com>
2024-01-27 14:31:08 -0500

    fix(neovim): Allow 2-space trailing whitespace in Markdown docs

--- neovim/.config/nvim/lua/justinfirsching/autocmds.lua
+++ neovim/.config/nvim/lua/justinfirsching/autocmds.lua
@@ -1,5 +1,17 @@
 -- Clear trailing whitespace on save
-vim.cmd('autocmd BufWritePre * %s/\\s\\+$//e')
+vim.api.nvim_create_autocmd({'BufWritePre'}, {
+    pattern = { "*" },
+    callback = function(ev)
+        if vim.bo.filetype ~= 'markdown' then
+            vim.api.nvim_command('%s/\\s\\+$//e')
+        else
+            -- Only replace trailing whitespace if it isn't exactly two spaces
+            -- which is used to force a line break
+            vim.api.nvim_command('%s/\\(\\S\\)\\s$/\\1/e')
+            vim.api.nvim_command('%s/\\s\\{3,\\}$//e')
+        end
+    end
+})
 
 -- Load updates to Xresources
 vim.cmd('autocmd BufWritePost .Xresources silent !xrdb <afile>')
