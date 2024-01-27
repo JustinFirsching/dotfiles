21b089f09145ec3d189f5a4a35ff5197e5e58c9e
Justin Firsching <JustinFirsching@gmail.com>
2024-01-27 14:33:11 -0500

    feat(neovim): Add bind to toggle search highlight

--- neovim/.config/nvim/lua/justinfirsching/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/keybinds.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 
 -- Exit Insert Mode
 vim.keymap.set('i', '<C-c>', '<Esc>')
+
+-- Toggle search highlighting
+vim.keymap.set('n', '<leader>hl', function()
+    vim.opt.hlsearch = not(vim.opt.hlsearch:get())
+end)
