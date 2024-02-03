52710f12b486dcefea13f02d8358fa2ef19a79a3
Justin Firsching <JustinFirsching@gmail.com>
2024-02-03 00:39:06 -0500

    fix(sh): Intelligently set GOROOT

--- sh/.config/sh/go.sh
+++ sh/.config/sh/go.sh
@@ -1,3 +1,4 @@
-export GOROOT=/usr/local/go
-export PATH=$GOROOT/bin:$PATH
+export GOROOT=$(GOROOT= go env GOROOT 2>/dev/null)
+
+[ ! -z "$GOROOT" ] && export PATH=$GOROOT/bin:$PATH
 export PATH=$HOME/go/bin:$PATH
