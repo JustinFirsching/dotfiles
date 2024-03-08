59c4ebd9bbc3f7d620e9f3cbe78c678d52a758f7
Justin Firsching <JustinFirsching@gmail.com>
2024-03-08 13:56:02 -0500

    feat(neovim): Maintain clipboard when pasting in visual mode

--- neovim/.config/nvim/lua/justinfirsching/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/keybinds.lua
@@ -7,3 +7,7 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 vim.keymap.set('n', '<leader>hl', function()
     vim.opt.hlsearch = not(vim.opt.hlsearch:get())
 end)
+
+-- Remap 'p' and 'P' in Visual Mode to not lose the pasted text
+vim.api.nvim_set_keymap('x', 'p', [[pgvy`>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', 'P', [[Pgvy`>]], { noremap = true, silent = true })
