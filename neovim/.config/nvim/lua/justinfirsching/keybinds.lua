44d308a4c572a52902c65cd79b018852096193dd
Justin Firsching <JustinFirsching@gmail.com>
2024-10-04 13:01:49 -0400

    feat(neovim): Go to new window after a split

--- neovim/.config/nvim/lua/justinfirsching/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/keybinds.lua
@@ -5,9 +5,17 @@ vim.keymap.set('i', '<C-c>', '<Esc>')
 
 -- Toggle search highlighting
 vim.keymap.set('n', '<leader>hl', function()
-    vim.opt.hlsearch = not(vim.opt.hlsearch:get())
+    vim.opt.hlsearch = not (vim.opt.hlsearch:get())
 end)
 
 -- Remap 'p' and 'P' in Visual Mode to not lose the pasted text
 vim.api.nvim_set_keymap('x', 'p', [[pgvy`>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', 'P', [[Pgvy`>]], { noremap = true, silent = true })
+
+-- Always move the the new buffer after a split
+local split_and_move = function(split_cmd)
+    vim.cmd(split_cmd)
+    vim.cmd("wincmd w")
+end
+vim.keymap.set({'v', 'n', 'o'}, '<C-W>s', function() split_and_move("split") end, { noremap = true, silent = true })
+vim.keymap.set({'v', 'n', 'o'}, '<C-W>v', function() split_and_move("vsplit") end, { noremap = true, silent = true })
