32a764853e5f9ea8d45da69f37652eda4443f5f3
Justin Firsching <JustinFirsching@gmail.com>
2025-03-09 15:43:30 -0400

    chore(neovim): Clean up keybinds

--- neovim/.config/nvim/lua/justinfirsching/keybinds.lua
+++ neovim/.config/nvim/lua/justinfirsching/keybinds.lua
@@ -1,6 +1,14 @@
-vim.g.mapleader = " "
-
 local map_key = require("justinfirsching.utils").map_key
+
+local split_and_move = function(split_cmd, callback)
+    vim.cmd(split_cmd)
+    vim.cmd("wincmd w")
+    if callback ~= nil then
+        callback()
+    end
+end
+
+vim.g.mapleader = " "
 
 -- Exit Insert Mode
 map_key('i', '<C-c>', '<Esc>')
@@ -10,16 +18,11 @@ map_key('n', '<leader>hl', function()
     vim.opt.hlsearch = not (vim.opt.hlsearch:get())
 end)
 
-
 -- Remap 'p' and 'P' in Visual Mode to not lose the pasted text
 map_key('x', 'p', [[pgvy`>]])
 map_key('x', 'P', [[Pgvy`>]])
 
 -- Always move the the new buffer after a split
-local split_and_move = function(split_cmd)
-    vim.cmd(split_cmd)
-    vim.cmd("wincmd w")
-end
 map_key({ 'v', 'n', 'o' }, '<C-W>s', function() split_and_move("split") end)
 map_key({ 'v', 'n', 'o' }, '<C-W>v', function() split_and_move("vsplit") end)
 
@@ -37,15 +40,10 @@ map_key("n", "]d", vim.diagnostic.goto_next)
 map_key("n", "[d", vim.diagnostic.goto_prev)
 
 -- LSP
-local split_to_definition = function(split_cmd)
-    vim.cmd(split_cmd)
-    vim.cmd("wincmd w")
-    vim.lsp.buf.definition()
-end
 map_key("n", "<leader>ca", vim.lsp.buf.code_action)
 map_key("n", "<leader>gD", vim.lsp.buf.declaration)
-map_key("n", "<leader>gds", function() split_to_definition("split") end)
-map_key("n", "<leader>gdv", function() split_to_definition("vsplit") end)
+map_key("n", "<leader>gds", function() split_and_move("split", vim.lsp.buf.definition) end)
+map_key("n", "<leader>gdv", function() split_and_move("vsplit", vim.lsp.buf.definition) end)
 map_key("n", "K", vim.lsp.buf.hover)
 map_key("n", "<leader>rn", vim.lsp.buf.rename)
 map_key("n", "<leader>sd", vim.diagnostic.open_float)
