2b722750bd6287b682bd5e9b82f0d6849f064311
Justin Firsching <JustinFirsching@gmail.com>
2025-08-12 21:08:09 -0400

    feat(sh): Add support for macOS pbcopy/pbpaste

--- sh/.config/sh/00.aliases.sh
+++ sh/.config/sh/00.aliases.sh
@@ -1,5 +1,16 @@
 alias ls='ls --color=auto'
 alias sl='ls --color=auto'
 alias grep='grep --color=auto'
-alias copy='xclip -i -sel clip'
-alias paste='xclip -o -sel clip'
+
+
+# If we have xclip installed, set up copy and paste aliases
+if command -v xclip &> /dev/null; then
+    alias copy='xclip -i -sel clip'
+    alias paste='xclip -o -sel clip'
+fi
+
+# If we have pbcopy and pbpaste (macOS) installed, set up copy and paste aliases
+if command -v pbcopy &> /dev/null && command -v pbpaste &> /dev/null; then
+    alias copy='pbcopy'
+    alias paste='pbpaste'
+fi
