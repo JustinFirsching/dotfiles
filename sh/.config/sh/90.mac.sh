14914c963b95f2b836bc8be413bb83bcd7f5ad62
Justin Firsching <JustinFirsching@gmail.com>
2025-08-12 21:08:32 -0400

    feat(sh): Add homebrew and gnu-tools support for macOS

---
+++ sh/.config/sh/90.mac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -f /opt/homebrew/bin/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if [ -d /opt/homebrew/opt/findutils ]; then
+    export PATH="/opt/homebrew/opt/findutils/libexec/gnubin:$PATH"
+fi
+
+if [ -d /opt/homebrew/opt/coreutils ]; then
+    export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+fi
