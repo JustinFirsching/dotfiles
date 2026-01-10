eb31e99ea200859c843374b95b2fb23f174a9c5d
Justin Firsching <JustinFirsching@gmail.com>
2026-01-10 16:17:03 -0500

    docs(sh): Add nvm install command reference

--- sh/.config/sh/10.nvm.sh
+++ sh/.config/sh/10.nvm.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 if [ -d "$HOME/.nvm" ]; then
     [ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
     source $NVM_DIR/nvm.sh
