dfa311969fff4e33ce5d9e81f025a4d8aaff922d
Justin Firsching <JustinFirsching@gmail.com>
2026-09-12 01:04:16 -0400

    feat(neovim): use native diagnostic floats

--- neovim/.config/nvim/lua/justinfirsching/options.lua
+++ neovim/.config/nvim/lua/justinfirsching/options.lua
@@ -1,5 +1,3 @@
-local lsp_ui = require("justinfirsching.lsp_ui")
-
 vim.opt.autoindent = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.colorcolumn = '80'
@@ -25,17 +23,4 @@ vim.opt.showmode = false
 vim.opt.hlsearch = false
 vim.opt.completeopt = 'menu,menuone,noselect,noinsert,preview'
 vim.opt.mouse = 'a'
-
-vim.diagnostic.config({
-    float = lsp_ui.diagnostic_float,
-})
-
-vim.lsp.handlers["textDocument/hover"] = lsp_ui.with_handler_opts(
-    vim.lsp.handlers.hover,
-    lsp_ui.hover
-)
-
-vim.lsp.handlers["textDocument/signatureHelp"] = lsp_ui.with_handler_opts(
-    vim.lsp.handlers.signature_help,
-    lsp_ui.signature_help
-)
+vim.opt.winborder = 'rounded'
